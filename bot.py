32ae519f4efc68b1475031960dbb0f742c1701a5
CrubBucket <80716486+CrubBucket@users.noreply.github.com>
2022-03-29 00:23:07 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,92 @@
+import sys
+import discord
+from discord.ext import commands
+from config import settings
+from random import choice
+
+
+bot = commands.Bot(command_prefix=settings['prefix'])
+#client = discord.Client()
+
+
+
+
+
+async def spam(channel):
+    gena = await bot.fetch_user(291212394120871936)
+    with open('games_to_comment.txt', 'r') as file:
+        lines = file.readlines()
+    for i in range(len(lines) - 2, 6, -9):
+        name = lines[i - 7][:-1]
+        op_name = lines[i - 6][:-1]
+        result = lines[i - 5][:-1]
+        color = lines[i - 4][:-1]
+        time_control = lines[i - 3][:-1]
+        rating = lines[i - 2][:-1]
+        op_rating = lines[i - 1][:-1]
+        game_id = lines[i][:-1]
+        if result == 'lose':
+            with open('lose_appeal.txt', 'r', encoding = 'UTF-8') as file:
+                troll = file.readlines()
+            main_output = f'{choice(troll[0:4])[0:-1]} {gena.mention} {choice(troll[4:8])[0:-1]} {choice(troll[8:12])[0:-1]} **{op_name}** в {time_control}! **{choice(troll[12:])[0:-1]}**'
+            pts_output = f'Теперь у него всего лишь **{rating} PTS\'ов**, а у оппонента аж **{op_rating}!**'
+            id_output = f'**ID каточки:** {game_id}'
+            sus_output = discord.Embed(title="ЛУЗ!", description=main_output + '\n' + '\n' + pts_output + '\n' + '\n' + id_output, colour=0xf80000)
+            await channel.send(embed=sus_output)
+        if result == 'win':
+            with open('win_appeal.txt', 'r', encoding = 'UTF-8') as file:
+                troll = file.readlines()
+            main_output = f'{choice(troll[0:4])[0:-1]} {gena.mention} {choice(troll[4:8])[0:-1]} {choice(troll[8:12])[0:-1]} **{op_name}** в {time_control}! **{choice(troll[12:])[0:-1]}**'
+            pts_output = f'Теперь у него аж **{rating} PTS\'ов**, а у оппонента всего **{op_rating}!**'
+            ebalo_output = f'Ну что, **{op_name}** - **ЕБАЛО СВОЕ ПРЕДСТАВИЛ?!**'
+            id_output = f'**ID каточки:** {game_id}'
+            sus_output = discord.Embed(title="ВИН!",
+                                       description=main_output + '\n' + '\n' + pts_output + '\n' + '\n' + ebalo_output+ '\n' + '\n' + id_output,
+                                       colour=0x00e600)
+            await channel.send(embed=sus_output)
+        if result == 'draw':
+            id_output = f'**ID каточки:** {game_id}'
+            sus_output = discord.Embed(title="НИЧЬЯ!",
+                                       description = f'У {gena.mention} и **{op_name}** письки оказались одного размера' '\n' + '\n' + id_output,
+                                       colour=0x6c6874)
+            await channel.send(embed=sus_output)
+    sys.exit()
+
+
+@bot.event
+async def on_ready():
+    print('Я в деле!\nP.S. {0.user}'.format(bot))
+    text_channel_list = []
+    for guild in bot.guilds:
+        for channel in guild.text_channels:
+            text_channel_list.append(channel)
+    with open('channel_id.txt', 'r') as file:
+        channel_id = file.read()
+        if channel_id == '':
+            out = discord.Embed(title = 'Куда высерать?', description = 'Напиши **~suda #(канал для высеров) **', colour = 0x6699cc)
+            await text_channel_list[0].send(embed = out)
+        else:
+            for guild in bot.guilds:
+                for channel in guild.text_channels:
+                    if channel.id == int(channel_id):
+                        #await channel.send('Ща высру')
+                        await spam(channel)
+
+
+@bot.command()
+async def suda(ctx, channel: discord.TextChannel):
+    with open('channel_id.txt', 'w') as file:
+        file.write(str(channel.id))
+    await channel.send('Ок')
+    await spam(channel)
+
+
+@bot.command()
+async def test(ctx):
+    print('Получил!')
+    await ctx.send(f'Я в норме!')
+
+
+bot.run(settings['token'])
+
+
---
+++ parsing.py
@@ -0,0 +1,106 @@
+import asyncio
+import os
+import time
+import requests
+from chessdotcom import get_player_game_archives
+
+name = 'thegreatcombinaator'
+
+
+def test(name):
+    #month_games = requests.get(get_player_game_archives(name).json['archives'][-1]).json()
+    data = get_player_game_archives(name).json
+    url = data['archives'][-1]
+    # month_games = requests.get(url).json()['games']
+    # month_games = requests.get(url).json()
+    #game = month_games['games'][-1]
+    print(url)
+    # print('Time controll:', game['time_control'])          # TIME CONTROL
+    # print('Time class:', game['time_class'])               # TIME CLASS
+    # print('Rules:', game['rules'])                         # RULES
+    # print('Rated?:', game['rated'])                        # RATED?
+    # print('White result:', game['white']['result'])        # WHITE RESULT
+    # print('White username:', game['white']['username'])    # WHITE USERNAME
+    # print('White rating:', game['white']['rating'])        # WHITE RATING
+    # print(month_games['games'])
+    # print(len(month_games['games']))
+    # game_id = game['url'].split('/')[-1]                   # GAME ID
+    # print(game_id)
+    # # with open('last_id.txt', 'w') as file:
+    # #     file.write(game_id)
+
+
+def get_missing(name):
+    missing_games = []
+    with open('last_id.txt', 'r') as file:
+        last_id = file.read()
+    latest_games = (requests.get(get_player_game_archives(name).json['archives'][-1]).json())['games']
+    for i in range(len(latest_games) - 1, 0, -1):
+        if latest_games[i]['url'].split('/')[-1] == last_id:
+            break
+        missing_games.append(latest_games[i])
+    last_id = latest_games[-1]['url'].split('/')[-1]
+    with open('last_id.txt', 'w') as file:
+        file.write(last_id)
+    return missing_games
+
+
+def shall_awake(missing):
+    if len(missing) == 0:
+        return 0
+    return 1
+
+
+def games_append(games, name):
+    with open('games_to_comment.txt', 'w') as file:
+        for game in games:
+            if game['rules'] == 'chess' and game['rated']:
+########################################  НИКИ   #######################################################################
+                file.write(name + '\n')
+                if game['white']['username'] != name:
+                    file.write(game['white']['username'] + '\n')
+                else:
+                    file.write(game['black']['username'] + '\n')
+###################################  Результат  ########################################################################
+                if ((game['white']['result'] == 'insufficient') or (game['white']['result'] == 'repetition')) and game['white']['username'] == name:
+                    file.write('draw\nwhite\n')
+                elif ((game['white']['result'] == 'insufficient') or (game['white']['result'] == 'repetition')) and game['black']['username'] == name:
+                    file.write('draw\nblack\n')
+                elif game['white']['result'] == 'win' and game['white']['username'] == name:
+                    file.write('win\nwhite\n')
+                elif game['white']['result'] != 'win' and game['white']['username'] == name:
+                    file.write('lose\nwhite\n')
+                elif game['black']['result'] == 'win':
+                    file.write('win\nblack\n')
+                else:
+                    file.write('lose\nblack\n')
+
+
+#insufficient
+#repetition
+#######################################################################################################################
+                file.write(game['time_class'] + '\n')               #Тайм - контроль
+#######################################################################################################################
+                if game['white']['username'] == name:
+                    file.write(str(game['white']['rating']) + '\n')     #Рейтинг
+                    file.write(str(game['black']['rating']) + '\n')
+                else:
+                    file.write(str(game['black']['rating']) + '\n')
+                    file.write(str(game['white']['rating']) + '\n')
+                file.write(game['url'].split('/')[-1] + '\n')
+                file.write('#' + '\n')
+
+
+while True:
+    games_to_comment = get_missing(name)
+    if shall_awake(games_to_comment):
+        games_append(games_to_comment, name)
+        os.startfile(r'bot.py')
+        time.sleep(300)
+        continue
+    time.sleep(30)
+
+
+
+
+
